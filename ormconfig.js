9612c9e8e161cd674e55c56aa6da33d748fa1c10
Rafael <rafaelcodomingues@gmail.com>
2020-09-06 18:04:43 -0300

    :tada: Preparing to deploy

---
+++ ormconfig.js
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+
+module.exports = {
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: 5432,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: ['./src/models/*.ts'],
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
+};
