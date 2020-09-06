7bbe970196c9f99898a2e122e1501aa2d1ba0080
Rafael <rafaelcodomingues@gmail.com>
2020-09-06 18:50:28 -0300

    :tada: Preparing to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -7,8 +7,14 @@ module.exports = {
   username: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  entities: ['./src/models/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
+  entities: [
+    process.env.NODE_ENV === 'dev' ? './src/models/*.ts' : './dist/models/*.js',
+  ],
+  migrations: [
+    process.env.NODE_ENV === 'dev'
+      ? './src/database/migrations/*.ts'
+      : './dist/database/migrations/*.js',
+  ],
   cli: {
     migrationsDir: './src/database/migrations',
   },
